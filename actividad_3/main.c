3f18d27b8e08a55fddbe7f4a3042194dd76eded9
Daniel Salazar <dsnile.salazr@gmail.com>
2025-04-17 23:16:06 -0600

    refactor

--- actividad_3/main.c
+++ actividad_3/main.c
@@ -35,153 +35,153 @@
 #define BUZZER_PIN  PORTC5
 
 typedef enum {
-    IDLE,           
-    CONFIG_SEC,     
-    CONFIG_MIN,     
-    RUNNING,        
-    PAUSED,         
-    ALARM           
-} State;
+      INIT_STATE,           
+      CONFIG_SEC,     
+      CONFIG_MIN,     
+      RUNNING,        
+      PAUSED,         
+      ALARM           
+  } State;
 
-volatile State current_state = IDLE;
-volatile uint8_t minutes = 59;
-volatile uint8_t seconds = 59;
-volatile uint8_t current_digit = 0;
-volatile uint8_t blink_state = 0;
-volatile uint16_t blink_counter = 0;
-volatile uint8_t buzzer_state = 0;
-volatile uint16_t buzzer_counter = 0;
+  volatile State current_state = INIT_STATE;
+  volatile uint8_t minutes = 59;
+  volatile uint8_t seconds = 59;
+  volatile uint8_t current_digit = 0;
+  volatile uint8_t blink_state = 0;
+  volatile uint16_t blink_counter = 0;
+  volatile uint8_t buzzer_state = 0;
+  volatile uint16_t buzzer_counter = 0;
 
-volatile uint8_t button0_flag = 0;
-volatile uint8_t button1_flag = 0;
-volatile uint8_t debounce_counter0 = 0;
-volatile uint8_t debounce_counter1 = 0;
-volatile uint8_t button0_pressed = 0;
-volatile uint8_t button1_pressed = 0;
+  volatile uint8_t button0_flag = 0;
+  volatile uint8_t button1_flag = 0;
+  volatile uint8_t debounce_counter0 = 0;
+  volatile uint8_t debounce_counter1 = 0;
+  volatile uint8_t button0_pressed = 0;
+  volatile uint8_t button1_pressed = 0;
 
-const uint8_t digit_patterns[10] = {
-    0b00111111,
-    0b00000110,
-    0b01011011,
-    0b01001111,
-    0b01100110,
-    0b01101101,
-    0b01111101,
-    0b00000111,
-    0b01111111,
-    0b01101111
-};
+  const uint8_t digit_patterns[10] = {
+      0b00111111,
+      0b00000110,
+      0b01011011,
+      0b01001111,
+      0b01100110,
+      0b01101101,
+      0b01111101,
+      0b00000111,
+      0b01111111,
+      0b01101111
+  };
 
-void set_digit(uint8_t digit) {
-    DISPLAY7SEG_AF_PORTX &= ~0x3F; 
-    DISPLAY7SEG_AF_PORTX |= (digit & 0x3F); 
-    
-    if (digit & 0x40) {
-        DISPLAY7SEG_G_PORTX |= (1 << DISPLAY7SEG_G);
-    } else {
-        DISPLAY7SEG_G_PORTX &= ~(1 << DISPLAY7SEG_G);
-    }
-}
+  void show_number(uint8_t digit) {
+      DISPLAY7SEG_AF_PORTX &= ~0x3F; 
+      DISPLAY7SEG_AF_PORTX |= (digit & 0x3F); 
+      
+      if (digit & 0x40) {
+          DISPLAY7SEG_G_PORTX |= (1 << DISPLAY7SEG_G);
+      } else {
+          DISPLAY7SEG_G_PORTX &= ~(1 << DISPLAY7SEG_G);
+      }
+  }
 
-void update_display() {
-    DISPLAY7SEG_MUX_PORTX &= ~((1 << DISPLAY7SEG_MUX_MILLARES) | 
-                              (1 << DISPLAY7SEG_MUX_CENTENAS) | 
-                              (1 << DISPLAY7SEG_MUX_DECENAS) | 
-                              (1 << DISPLAY7SEG_MUX_UNIDADES));
-    
-    uint8_t show_sec_digits = 1;
-    uint8_t show_min_digits = 1;
-    
-    if (current_state == CONFIG_SEC) {
-        show_sec_digits = blink_state;
-    }
-    else if (current_state == CONFIG_MIN) {
-        show_min_digits = blink_state;
-    }
-    
-    switch(current_digit) {
-        case 0: 
-            if (show_min_digits) {
-                set_digit(digit_patterns[minutes / 10]);
-                DISPLAY7SEG_MUX_PORTX |= (1 << DISPLAY7SEG_MUX_MILLARES);
-            }
-            break;
-            
-        case 1:
-            if (show_min_digits) {
-                set_digit(digit_patterns[minutes % 10]);
-                DISPLAY7SEG_MUX_PORTX |= (1 << DISPLAY7SEG_MUX_CENTENAS);
-            }
-            break;
-            
-        case 2:
-            if (show_sec_digits) {
-                set_digit(digit_patterns[seconds / 10]);
-                DISPLAY7SEG_MUX_PORTX |= (1 << DISPLAY7SEG_MUX_DECENAS);
-            }
-            break;
-            
-        case 3:
-            if (show_sec_digits) {
-                set_digit(digit_patterns[seconds % 10]);
-                DISPLAY7SEG_MUX_PORTX |= (1 << DISPLAY7SEG_MUX_UNIDADES);
-            }
-            break;
-    }
-    
-    current_digit = (current_digit + 1) % 4;
-    blink_counter++;
-    if (blink_counter >= 20) {
-        blink_state = !blink_state;
-        blink_counter = 0;
-    }
-}
-void process_button0_action() {
-    switch (current_state) {
-        case IDLE:
-            current_state = RUNNING;
-            break;
-            
-        case CONFIG_SEC:
-            seconds = (seconds + 1) % 60;
-            break;
-            
-        case CONFIG_MIN:
-            minutes = (minutes + 1) % 60;
-            break;
-            
-        case RUNNING:
-            current_state = PAUSED;
-            break;
-            
-        case PAUSED:
-            current_state = RUNNING;
-            break;
-            
-        case ALARM:
-            current_state = IDLE;
-            minutes = 59;
-            seconds = 59;
-            break;
-    }
-}
+  void update_temp() {
+      DISPLAY7SEG_MUX_PORTX &= ~((1 << DISPLAY7SEG_MUX_MILLARES) | 
+                                (1 << DISPLAY7SEG_MUX_CENTENAS) | 
+                                (1 << DISPLAY7SEG_MUX_DECENAS) | 
+                                (1 << DISPLAY7SEG_MUX_UNIDADES));
+      
+      uint8_t show_sec_digits = 1;
+      uint8_t show_min_digits = 1;
+      
+      if (current_state == CONFIG_SEC) {
+          show_sec_digits = blink_state;
+      }
+      else if (current_state == CONFIG_MIN) {
+          show_min_digits = blink_state;
+      }
+      
+      switch(current_digit) {
+          case 0: 
+              if (show_min_digits) {
+                  show_number(digit_patterns[minutes / 10]);
+                  DISPLAY7SEG_MUX_PORTX |= (1 << DISPLAY7SEG_MUX_MILLARES);
+              }
+              break;
+              
+          case 1:
+              if (show_min_digits) {
+                  show_number(digit_patterns[minutes % 10]);
+                  DISPLAY7SEG_MUX_PORTX |= (1 << DISPLAY7SEG_MUX_CENTENAS);
+              }
+              break;
+              
+          case 2:
+              if (show_sec_digits) {
+                  show_number(digit_patterns[seconds / 10]);
+                  DISPLAY7SEG_MUX_PORTX |= (1 << DISPLAY7SEG_MUX_DECENAS);
+              }
+              break;
+              
+          case 3:
+              if (show_sec_digits) {
+                  show_number(digit_patterns[seconds % 10]);
+                  DISPLAY7SEG_MUX_PORTX |= (1 << DISPLAY7SEG_MUX_UNIDADES);
+              }
+              break;
+      }
+      
+      current_digit = (current_digit + 1) % 4;
+      blink_counter++;
+      if (blink_counter >= 20) {
+          blink_state = !blink_state;
+          blink_counter = 0;
+      }
+  }
+  void Button0_action() {
+      switch (current_state) {
+          case INIT_STATE:
+              current_state = RUNNING;
+              break;
+              
+          case CONFIG_SEC:
+              seconds = (seconds + 1) % 60;
+              break;
+              
+          case CONFIG_MIN:
+              minutes = (minutes + 1) % 60;
+              break;
+              
+          case RUNNING:
+              current_state = PAUSED;
+              break;
+              
+          case PAUSED:
+              current_state = RUNNING;
+              break;
+              
+          case ALARM:
+              current_state = INIT_STATE;
+              minutes = 59;
+              seconds = 59;
+              break;
+      }
+  }
 
-void process_button1_action() {
-    switch (current_state) {
-        case IDLE:
-            current_state = CONFIG_SEC;
-            break;
-            
-        case CONFIG_SEC:
-            current_state = CONFIG_MIN;
-            break;
-            
-        case CONFIG_MIN:
-            current_state = IDLE;
-            break;
-            
-        case ALARM:
-            current_state = IDLE;
+  void Button1_action() {
+      switch (current_state) {
+          case INIT_STATE:
+              current_state = CONFIG_SEC;
+              break;
+              
+          case CONFIG_SEC:
+              current_state = CONFIG_MIN;
+              break;
+              
+          case CONFIG_MIN:
+              current_state = INIT_STATE;
+              break;
+              
+          case ALARM:
+            current_state = INIT_STATE;
             minutes = 59;
             seconds = 59;
             break;
@@ -209,14 +209,13 @@ void init() {
     EIMSK |= (1 << INT0) | (1 << INT1);
     
     TCCR0A |= (1 << WGM01);
-    TCCR0B |= (1 << CS01) | (1 << CS00);  
-    OCR0A = 100;  
-    TIMSK0 |= (1 << OCIE0A);
-
-
-    TCCR1B |= (1 << WGM12) | (1 << CS12);  
-    OCR1A = 62499;  
-    TIMSK1 |= (1 << OCIE1A);
+	TCCR0B |= (1 << CS01) | (1 << CS00);  
+	OCR0A = 100;  
+	TIMSK0 |= (1 << OCIE0A);
+    
+ 
+	TCCR1B |= (1 << WGM12) | (1 << CS12); 
+	OCR1A = 62499;  
     
     TCCR2A |= (1 << WGM21);
     TCCR2B |= (1 << CS22);
@@ -227,7 +226,7 @@ void init() {
 }
 
 ISR(TIMER0_COMPA_vect) {
-    update_display();
+    update_temp();
 }
 
 ISR(TIMER1_COMPA_vect) {
@@ -298,12 +297,12 @@ int main(void) {
     
     while (1) {
         if (button0_pressed) {
-            process_button0_action();
+            Button0_action();
             button0_pressed = 0;
         }
         
         if (button1_pressed) {
-            process_button1_action();
+            Button1_action();
             button1_pressed = 0;
         }
     }
